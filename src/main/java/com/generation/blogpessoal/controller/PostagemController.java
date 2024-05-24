beba7e9f4d36658ec514bec95ebe64be9c633846
isabellyperez <isabellyperez10@gmail.com>
2024-05-24 18:32:07 -0300

    Atualizados os metodos post e put na Classe PostagemController

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -14,11 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -29,6 +31,9 @@ import jakarta.validation.Valid;
 public class PostagemController {
 	@Autowired //injeção de dependencias - instanciar a classe PostagemRepository
 	private PostagemRepository postagemRepository;
+	
+	@Autowired 
+	private TemaRepository temaRepository;
 	
 	@GetMapping //defini e verbo http que atende esse metodo
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -55,26 +60,31 @@ public class PostagemController {
 	//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título", "Texto", "2024-12-31 14:05:01");
 	@PostMapping //anotação PostMapping para ficar com o verbo correspondente com o que vamos executar - um cadastro novo
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-
+		if (temaRepository.existsById(postagem.getTema().getId()))
 		//retorno em formato ResponseEntity
 		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(postagemRepository.save(postagem));
+				.body(postagemRepository.save(postagem));	
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
 
 	}
 	
 	//UPDATE tb_postagem SET titulo = "...", texto = "..." Where id = 1
 	@PutMapping //verbo http Put para o consumo no insomnia
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta-> ResponseEntity.status(HttpStatus.OK)
-				.body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-
+		if (postagemRepository.existsById(postagem.getId())) {
+			if (temaRepository.existsById(postagem.getTema().getId())) {
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+			}
+			
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O Tema não existe!", null);
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	//DELETE FROM tb_postagens WHERE id = id;
-	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id) {
 		Optional<Postagem> postagem = postagemRepository.findById(id);
